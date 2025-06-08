7c6be41abd968d55a5b75abebed528774714216b
LukaszSwiech <luka.swiech@gmail.com>
2025-06-08 20:27:43 +0200

    Logic in statements. Lesson 3 assessments.

---
+++ ProcessStringContent/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation = 0;
+string sentence = string.Empty;
+
+//foreach (string myString in myStrings)
+
+for (int i = 0; i < myStrings.Length; i++)
+{
+    periodLocation = myStrings[i].IndexOf(".");
+
+    while (periodLocation != -1)
+    {
+        sentence = myStrings[i].Substring(0, periodLocation).Trim();
+        myStrings[i] = myStrings[i].Remove(0, periodLocation + 1);
+        Console.WriteLine(sentence);
+        periodLocation = myStrings[i].IndexOf('.');
+    }
+
+    sentence = myStrings[i].Trim();
+    Console.WriteLine(sentence);
+}
---
+++ ValidateUserInput/Program.cs
@@ -0,0 +1,57 @@
+﻿// // CODE PROJECT 1: write code that validates integer input
+
+// string? userInput;
+// bool validNumber;
+// bool acceptedValue = false;
+
+// Console.WriteLine("Please enter a number between 5 and 10:");
+
+// do
+// {
+//     userInput = Console.ReadLine();
+//     validNumber = int.TryParse(userInput, out int numericValue);
+
+//     if (validNumber)
+//     {
+//         if (numericValue >= 5 && numericValue <= 10)
+//         {
+//             Console.WriteLine($" Your number ({numericValue}) is valid.");
+//             acceptedValue = true;
+//         }
+//         else
+//         {
+//             Console.WriteLine($"You enterer {numericValue}, which is not in the range of 5 to 10. Please try again.");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Invalid number, please try again.");
+//     }
+
+
+// } while (!acceptedValue);
+
+
+// CODE PROJECT 2:  write code that validates string input
+
+
+string? userInput;
+bool validString = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User):");
+
+do
+{
+    userInput = Console.ReadLine();
+
+    if (userInput == "Administrator" || userInput == "Manager" || userInput == "User")
+    {
+        validString = true;
+        Console.WriteLine($"Your role ({userInput}) has been accepted.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid role name, please try again.");
+    }
+
+} while (!validString);
