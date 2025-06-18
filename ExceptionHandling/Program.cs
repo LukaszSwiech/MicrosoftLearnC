cdaab12d1ffa6bd54182866efba323fe394d4105
LukaszSwiech <luka.swiech@gmail.com>
2025-06-18 17:44:31 +0200

    Exception Handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,177 @@
+﻿try
+{
+    checked
+    {
+        int num1 = int.MaxValue;
+        int num2 = int.MaxValue;
+        int result = num1 + num2;
+        Console.WriteLine("Result: " + result);
+    }
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
+}
+
+try
+{
+    string str = null;
+    int length = str.Length;
+    Console.WriteLine("String Length: " + length);
+}
+catch (NullReferenceException ex)
+{
+    Console.WriteLine("Error: The reference is null." + ex.Message);
+}
+
+try
+{
+    int[] numbers = new int[5];
+    numbers[5] = 10;
+    Console.WriteLine("Number at index 5: " + numbers[5]);
+
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine("Error: Index out of range." + ex.Message);
+}
+
+try
+{
+    int num3 = 10;
+    int num4 = 0;
+    int result2 = num3 / num4;
+    Console.WriteLine("Result: " + result2);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+}
+
+Console.WriteLine("Exiting program.");
+
+
+/* SEQUENCE OF CATCH
+// inputValues is used to store numeric values entered by a user
+string[] inputValues = new string[]{"three", "9999999999", "0", "2" };
+
+foreach (string inputValue in inputValues)
+{
+    int numValue = 0;
+    try
+    {
+        numValue = int.Parse(inputValue);
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Invalid readResult. Please enter a valid number.");
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine("The number you entered is too large or too small.");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+} */
+
+
+/* try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+    }
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+    byte smallNumber;
+
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
+    checked
+    {
+        try
+        {
+            smallNumber = (byte)number1;
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+        }
+    }
+} */
+
+
+
+/* EXCEPTION TYPES
+
+// ArrayTypeMismatchException
+string[] names = { "Dog", "Cat", "Fish" };
+Object[] objs = (Object[])names;
+
+Object obj = (Object)13;
+objs[2] = obj; // ArrayTypeMismatchException occurs
+
+// DivideByZeroException
+int number1 = 3000;
+int number2 = 0;
+Console.WriteLine(number1 / number2); // DivideByZeroException occurs
+
+// FormatException
+int valueEntered;
+string userValue = "two";
+valueEntered = int.Parse(userValue); // FormatException occurs
+
+//IndexOutOfRangeException
+int[] values1 = { 3, 6, 9, 12, 15, 18, 21 };
+int[] values2 = new int[6];
+
+values2[values1.Length - 1] = values1[values1.Length - 1]; // IndexOutOfRangeException occurs
+
+//InvalidCastException
+object obj = "This is a string";
+int num = (int)obj;
+
+//NullReferenceException
+int[] values = null;
+for (int i = 0; i <= 9; i++)
+    values[i] = i * 2;
+
+string? lowCaseString = null;
+Console.WriteLine(lowCaseString.ToUpper());
+
+//OverflowException
+decimal x = 400;
+byte i;
+
+i = (byte)x; // OverflowException occurs
+Console.WriteLine(i); */
